136bdd162df186c6df71adef1a589254f1863d79
Kamil Mucha <Kamil.Mucha@rossmann.com.pl>
2022-09-30 10:37:19 +0200

    Init

---
+++ StringCalculator.Test/StringCalculatorTest.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace StringCalculator.Test
+{
+
+    public class StringCalculatorTest
+    {
+        [Test]
+        public void ShouldReturnZeroForEmptyString()
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add(string.Empty);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ShouldReturnSameNumberForOneParameter()
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add("1");
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ShouldReturnSumForTwoParameters()
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add("2,5");
+            Assert.That(result, Is.EqualTo(7));
+        }
+
+        //[Test]
+        //public void ShouldThrowExceptionForThreeParameters()
+        //{
+        //    var calc = new StringCalculator();
+        //    //var result = calc.Add("2,5,4");
+        //    Assert.That(() => calc.Add("2,6,7"), Throws.TypeOf<ArgumentException>());
+        //}
+
+        [Test]
+        public void ShouldThrowExceptionWhenContainsInvalidCharacters()
+        {
+            var calc = new StringCalculator();
+            Assert.That(() => calc.Add("4,a"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => calc.Add("b,a,c"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => calc.Add("1.2.3"), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test] 
+        public void ShouldWorkForMoreParameters([Range(3,100)] int counter)
+        {
+            var calc = new StringCalculator();
+
+            var tempString = new List<string>();
+
+            for(int i =0; i< counter; i++)
+            {
+                tempString.Add("1");
+            }
+
+            var result = calc.Add(string.Join(',',tempString));
+            Assert.That(result, Is.EqualTo(counter));
+        }
+
+        [Test]
+        public void ShouldAddWhenNewLine()
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add("2\n5");
+            Assert.That(result, Is.EqualTo(7));
+
+        }
+
+        [Test]
+        public void ShouldAddWhenDiffrentDelimiters()
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add("2\n5,3");
+            Assert.That(result, Is.EqualTo(10));
+
+        }
+
+        [Test]
+        public void ShouldAddWhenCustomDelimiter()
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add("//;\n1;2");
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ShouldAddWhenCustomWithDefaultDelimiter()
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add("//;\n1;2,3\n4");
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenAddNegativeNumbers()
+        {
+            var calc = new StringCalculator();
+
+            Assert.That(() => calc.Add("-1,2,-3"), Throws.TypeOf<ArgumentException>().With.Message.Contains("-1,-3"));
+        }
+    }
+}
---
+++ StringCalculatorApp/StringCalculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string a)
+        {
+            if (string.IsNullOrWhiteSpace(a))
+            {
+                return 0;
+            }
+
+            var delimiters = ",\n";
+            
+            if (a.StartsWith("//"))
+            {
+                var firstLine = a.Split('\n')[0];
+                delimiters += firstLine.Substring(2, 1);
+                a = a.Replace(firstLine + "\n", "");
+            }
+
+            var numbers = a.Split(delimiters.ToCharArray());
+
+            var result = 0;
+            var tmpNumber = 0;
+            var negativeNumbers = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!int.TryParse(number, out tmpNumber))
+                {
+                    throw new ArgumentException();
+                }
+
+                if(tmpNumber < 0)
+                {
+                    negativeNumbers.Add(tmpNumber);
+                }
+
+                result += tmpNumber;
+            }
+
+            if(negativeNumbers.Any())
+            {
+                throw new ArgumentException(string.Join(',', negativeNumbers));
+            }
+
+            return result;
+
+        }
+    }
+}
